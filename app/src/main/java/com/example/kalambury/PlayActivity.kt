c3e24802c70456623e4c3f1ac7263a1141ba95a3
Blazej S <bsta@wp.pl>
2021-02-11 22:20:11 -0800

    serve random term

--- app/src/main/java/com/example/kalambury/PlayActivity.kt
+++ app/src/main/java/com/example/kalambury/PlayActivity.kt
@@ -3,23 +3,51 @@ package com.example.kalambury
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.core.text.HtmlCompat
+import com.example.kalambury.db.DatabaseHelper
+import kotlin.random.Random
 
 class PlayActivity : AppCompatActivity() {
+    lateinit var databaseHelper : DatabaseHelper
+    var lastGeneratedNumber : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play)
 
-/*        val rulesTextView = findViewById<TextView>(R.id.rulesTextView)*/
         val nextermButton = findViewById<Button>(R.id.nextTermButton)
-
-        /*rulesTextView.text = HtmlCompat.fromHtml(htmlText, 0);*/
+        databaseHelper = DatabaseHelper(this)
+        this.getNextTerm()
 
         nextermButton.setOnClickListener{
-            val i = Intent(this, MainActivity::class.java)
-            startActivity(i)
+            this.getNextTerm()
         }
     }
+
+    private fun getNextTerm()  {
+        val data =  databaseHelper.readData()
+        val rowsCount = data.count
+
+        var termTextView = findViewById<TextView>(R.id.termTextView)
+        var generatedNumber = generateNextRandomNumber(rowsCount)
+        lastGeneratedNumber = generatedNumber
+
+        Log.i("PlayActivity", generatedNumber.toString())
+        data.moveToPosition(generatedNumber)
+        termTextView.text = data.getString(1)
+    }
+
+    private fun generateNextRandomNumber(rowsCount: Int) : Int {
+        var generatedNumber = Random.nextInt(from = 0, until = rowsCount)
+
+        if(generatedNumber != lastGeneratedNumber){
+            return generatedNumber
+        }
+
+        return generateNextRandomNumber(rowsCount)
+    }
+
 }
